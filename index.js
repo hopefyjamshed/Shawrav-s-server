900ab4afda65a5dd2f9be6de068ca048887a342c
hopefyjamshed <103117529+hopefyjamshed@users.noreply.github.com>
2022-11-10 01:02:44 +0600

    get data by id added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -26,10 +26,28 @@ async function run() {
 
         app.get('/services', async (req, res) => {
             const query = {}
+
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.limit(3).toArray()
+
+            res.send(services)
+
+        })
+        app.get('/allservices', async (req, res) => {
+            const query = {}
+
             const cursor = serviceCollection.find(query)
             const services = await cursor.toArray()
 
             res.send(services)
+
+        })
+        app.get('/allservices/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const service = await serviceCollection.findOne(query)
+
+            res.send(service)
 
         })
 
